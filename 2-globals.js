51ca2b4950dec7dd923fd2612fadb51e8c1d07d4
Andrew S <andrewsheamls@gmail.com>
2022-03-10 16:38:24 -0700

    first commit

---
+++ 2-globals.js
@@ -0,0 +1,13 @@
+
+const john = `john`
+const peter = `peter`
+const secret = `SUPER SECRECT`
+
+const sayHi = (name) =>{
+    console.log(`Hello there ${name}`)
+}
+
+sayHi(`Andrew`)
+sayHi(`Dennis`)
+sayHi(john)
+sayHi(peter)
